b0cb8f2179fb96aa0ef2a45d802708057019a94d
Nan <linanenv@gmail.com>
2024-09-14 12:18:41 +0800

    Update .gitignore to include 'output' directory

---
+++ src/data_extract.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+for file in os.listdir("data/processes"):
+    with open("data/processes/" + file, "r") as f:
+        data = json.load(f)
+        # print(data)
+
+    output_data = {
+        "_index": "process_index",
+        "_source": {
+            "process_id": data["@id"],
+            "process_name": data["name"],
+            "process_location": data["location"]["name"],
+            "process_amount": None,
+            "process_unit": None,
+            "input_flows": [],
+            "output_flows": [],
+        },
+    }
+
+    for exchange in data.get("exchanges", []):
+        flow_id = exchange["flow"]["@id"]
+
+        if exchange["isInput"]:
+            output_data["_source"]["input_flows"].append(flow_id)
+        else:
+            output_data["_source"]["output_flows"].append(flow_id)
+
+        # 设定 process_amount 和 process_unit
+        if exchange.get("isQuantitativeReference"):
+            output_data["_source"]["process_amount"] = exchange["amount"]
+            output_data["_source"]["process_unit"] = exchange["unit"]["name"]
+
+    with open("output/processes/" + file, "w") as f:
+        json.dump(output_data, f, indent=2)
+
+    # print(output_data)
