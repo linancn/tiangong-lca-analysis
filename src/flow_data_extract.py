427743063757eebdb7a3929b5b0c9dd08b5a03d1
Nan <linanenv@gmail.com>
2024-09-14 12:26:43 +0800

    Refactor data extraction scripts for processes and flows

---
+++ src/flow_data_extract.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+for file in os.listdir("data/flows"):
+    with open("data/flows/" + file, "r") as f:
+        data = json.load(f)
+        # print(data)
+
+    output_data = {
+        "_index": "flow_index",
+        "_source": {
+            "flow_id": data["@id"],
+            "flow_name": data["name"],
+            "flow_type": data["flowType"],
+            "flow_unit": data["flowProperties"]["refUnit"],
+        },
+    }
+
+    with open("output/flows/" + file, "w") as f:
+        json.dump(output_data, f, indent=2)
+
+    # print(output_data)
